75078b2a94e0c9b159ca77b0a576092cd4f4164a
jameswhatcott <james@whatcott.com>
2024-05-20 22:31:41 -0500

    did first part of trying to fix back button

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,4 +1,6 @@
  
+// Getting the content on the page
+
  let username = localStorage.getItem('name');
  let title = localStorage.getItem('title');
  let content = localStorage.getItem('content');
@@ -7,3 +9,15 @@
  document.getElementById('usernameDisplay').textContent = 'Username: ' + username;
  document.getElementById('titleDisplay').textContent = 'Title: ' + title;
  document.getElementById('contentDisplay').textContent = 'Content: ' + content;
+
+
+ // Back button
+
+ let backBtn = document.getElementById('backBtn');
+
+ function comeBack (event) {
+    event.preventDefault();
+    window.location.href = 'index.html';
+    console.log('Redirecting back to main page');
+ }
+ backBtn.on('click', comeBack())
