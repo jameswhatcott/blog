a829c42d044fd7222efa4176f29a12f5cc4be85f
jameswhatcott <james@whatcott.com>
2024-05-23 10:45:24 -0500

    added alert

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -8,30 +8,37 @@ document.getElementById("blogForm").addEventListener("submit", function (event) 
     let title = document.getElementById('title').value;
     let content = document.getElementById('content').value;
 
-    let blogEntries = localStorage.getItem('blogEntries');
-    blogEntries = JSON.parse(blogEntries);
+    if (username === "" ||
+        title === "" ||
+        content === "") {
+            alert("Please don't leave any field blank")
+        } else {
+         let blogEntries = localStorage.getItem('blogEntries');
+         blogEntries = JSON.parse(blogEntries);
 
-    if (blogEntries === null) {
-        blogEntries = [];
-    }
+         if (blogEntries === null) {
+         blogEntries = [];
+         }
 
-    let blogEntry = {
-        username: username,
-        title: title,
-        content: content,
-    };
+        let blogEntry = {
+            username: username,
+            title: title,
+            content: content,
+            };
 
     // Add the new entry to the array
-    blogEntries.push(blogEntry);
-    blogEntries = JSON.stringify(blogEntries);
+        blogEntries.push(blogEntry);
+        blogEntries = JSON.stringify(blogEntries);
     // Save the updated array back to localStorage
-    localStorage.setItem('blogEntries', blogEntries);
-    console.log("Data stored in local storage");
+        localStorage.setItem('blogEntries', blogEntries);
+        console.log("Data stored in local storage");
 
 
     // Redirect to display.html
-    window.location.href = 'blog.html';
-    console.log("Redirecting to blog.html");
+        window.location.href = 'blog.html';
+        console.log("Redirecting to blog.html");
+
+        }
 
 });
 
